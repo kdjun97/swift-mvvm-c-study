dc42a8a1dc945b309ba880ee7cc2fb062215afcd
kdjun97 <kdjun97@gmail.com>
2024-08-03 00:46:31 +0900

    initial project commit

---
+++ MVVM-C-Pattern-Study/MVVM-C-Pattern-Study/MVVM_C_Pattern_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_C_Pattern_StudyApp.swift
+//  MVVM-C-Pattern-Study
+//
+//  Created by 김동준 on 8/3/24
+//
+
+import SwiftUI
+
+@main
+struct MVVM_C_Pattern_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
